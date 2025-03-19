060acd37f8ee146ef3c9b5aa47599f55d1b2ab32
vitedevteam <139409934+vitedevteam@users.noreply.github.com>
2025-03-19 06:42:56 +0000

    Fix deploy workflow

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ export default defineNuxtConfig({
   modules: [
     '@nuxt/content',
   ],
+  target: 'static',
   vue: {
     compilerOptions: {
       isCustomElement: (tag) => ['iconify-icon', 'swiper-container', 'swiper-slide'].includes(tag),
